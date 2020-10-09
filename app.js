b866a9673d6d3cdd7db54b966c85dd11a13ac244
Michelle Charron <michelle.charron93@gmail.com>
2020-10-09 16:54:05 -0600

    Styling changes and JS function

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //---Notes
-//4 upgrades with modifiers(1 passive and one active on-click)--- partial completion, need passive and click modifiers
-//display modifiers---
 
 //up-to-date count of resource--- completed, needs style
 let cheese = 0
 
 //upgrades have different modifiers---completed
+//4 upgrades with modifiers(1 passive and one active on-clicks--- completed 
 let clickUpgrades = {
   hammer: {
     price: 5,
@@ -36,7 +35,7 @@ let clickUpgrades = {
 //image that can be clicked for resource ---completed, needs style
 function onclickMine() {
   cheese++
-
+  
   mineModifier(false)
   update()
 }
@@ -48,14 +47,22 @@ function automMine(){
 
 function mineModifier(automatic) {
   for (const key in clickUpgrades) {
-      const item = clickUpgrades[key];  
-      if(item.automatic == automatic){ 
-        cheese += item.multiplier * item.quanity;
-      }
+    const item = clickUpgrades[key];  
+    if(item.automatic == automatic){ 
+      cheese += item.multiplier * item.quanity;
+    }
   }
 }
 
-
+function drawMultiplier(){
+  let modifier = 0
+  
+  for (const key in clickUpgrades) { 
+    const item = clickUpgrades[key];
+    modifier += item.multiplier * item.quanity
+  }
+  return modifier
+}
 
 //disable buttons for items if not enough resources--- completed, cannot increase if not enough money
 function buyClickItem(item) {
@@ -64,35 +71,40 @@ function buyClickItem(item) {
     clickUpgrades[item].quanity++
     priceIncrease(item)
   }
-
+  
+  drawMultiplier()
   update()
-}
-
-function modifierStack(tool) {
-
 }
 
 //price of upgrade increases after every purchase---completed
 function priceIncrease(purchase) {
   let double = clickUpgrades[purchase].price
   clickUpgrades[purchase].price += double
-  console.log(clickUpgrades[purchase].price)
-
+  
 }
 
-
+//display modifiers--- completed
 //draw function for page on load AND draw updates to page--- completed first half, need to update on modifiers
 function update() {
   let cheeseElem = document.getElementById("cheese")
   cheeseElem.innerText = 'Total: ' + cheese.toString()
-
+  
   let hammerPrice = document.getElementById('hammer')
   hammerPrice.innerHTML = 'Hammer: ' + clickUpgrades.hammer.price.toString()
-
+  
+  let starPrice = document.getElementById('star')
+  starPrice.innerHTML = 'Star Power: ' + clickUpgrades.star.price.toString()
+  
   let cloudPrice = document.getElementById('clouds')
   cloudPrice.innerHTML = 'Clouds: ' + clickUpgrades.clouds.price.toString()
 
+  let twinPrice = document.getElementById('twin')
+  twinPrice.innerHTML = 'Twin Kirby: ' + clickUpgrades.twin.price.toString()
+
+  let modifierElem = document.getElementById('modifiers')
+  modifierElem.innerHTML = 'Modifiers: ' + drawMultiplier()
 }
 
+setInterval(automMine, 4000)
+
 update()
-setInterval(automMine, 1000)
