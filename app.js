65d0a66d4ddcbaf172d1302d4af182aa510f2df1
Michelle Charron <michelle.charron93@gmail.com>
2020-10-10 13:55:38 -0600

    Styling Edits

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let cheese = 0
 //4 upgrades with modifiers(1 passive and one active on-clicks--- completed 
 let clickUpgrades = {
   hammer: {
-    price: 5,
+    price: 10,
     quanity: 0,
     multiplier: 1,
     automatic: false
@@ -15,17 +15,17 @@ let clickUpgrades = {
   star: {
     price: 50,
     quanity: 0,
-    multiplier: 3,
+    multiplier: 5,
     automatic: false
   },
   clouds: {
-    price: 10,
+    price: 400,
     quanity: 0,
     multiplier: 20,
     automatic: true
   },
   twin: {
-    price: 500,
+    price: 1000,
     quanity: 0,
     multiplier: 50,
     automatic: true
@@ -70,11 +70,17 @@ function buyClickItem(item) {
     cheese -= clickUpgrades[item].price
     clickUpgrades[item].quanity++
     priceIncrease(item)
+    let kirbyElem = document.getElementById('kirby')
+    kirbyElem.classList.add('power-up')
   }
   
+
+
   drawMultiplier()
   update()
 }
+
+
 
 //price of upgrade increases after every purchase---completed
 function priceIncrease(purchase) {
@@ -87,7 +93,7 @@ function priceIncrease(purchase) {
 //draw function for page on load AND draw updates to page--- completed first half, need to update on modifiers
 function update() {
   let cheeseElem = document.getElementById("cheese")
-  cheeseElem.innerText = 'Total: ' + cheese.toString()
+  cheeseElem.innerText = 'Total Kandy: ' + cheese.toString()
   
   let hammerPrice = document.getElementById('hammer')
   hammerPrice.innerHTML = 'Hammer: ' + clickUpgrades.hammer.price.toString()
@@ -103,6 +109,18 @@ function update() {
 
   let modifierElem = document.getElementById('modifiers')
   modifierElem.innerHTML = 'Modifiers: ' + drawMultiplier()
+
+  let hammerInvElem = document.getElementById('hammer-inv')
+  hammerInvElem.innerHTML = 'Hammers: ' + clickUpgrades.hammer.quanity.toString() + '<br> +1 click'
+
+  let starInvElem = document.getElementById('star-inv')
+  starInvElem.innerHTML = 'Stars: ' + clickUpgrades.star.quanity.toString() +'<br> +5 clicks'
+
+  let cloudsInvElem = document.getElementById('clouds-inv')
+  cloudsInvElem.innerHTML = 'Clouds: ' + clickUpgrades.clouds.quanity.toString() + '<br> +20 passive clicks'
+
+  let twinInvElem = document.getElementById('twin-inv')
+  twinInvElem.innerHTML = 'Twins: ' + clickUpgrades.twin.quanity.toString() + '<br> +50 passive clicks'
 }
 
 setInterval(automMine, 4000)
